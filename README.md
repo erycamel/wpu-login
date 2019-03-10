60a81c537fb0470e43c23b1b2d52c88a5bd59107
Ery Camel <erycamel@gmail.com>
2019-03-10 21:47:10 +0700

    video3

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Login ini, menyiapkan CodeIgniter, SB Admin 2 dan juga menyiapkan halaman untuk 
 
 --
 LINK : https://www.youtube.com/watch?v=8ROgaHfLRq0&list=PLFIM0718LjIXU8ul9FiN-owk04cQKtHPw
+
 CodeIgniter : https://codeigniter.com
+
 SBAdmin 2 : https://startbootstrap.com/themes/sb-admin-2/
+
 Bootstrap : https://getbootstrap.com/
+
 Font Awesome : https://fontawesome.com/
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,5 +1,3 @@
-
-
   <div class="container">
 
     <!-- Outer Row -->
@@ -16,18 +14,17 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Login Page!</h1>
                   </div>
+                  <?= $this->session->flashdata('message'); ?>
                   <form class="user">
                     <div class="form-group">
-                      <input type="text" class="form-control form-control-user" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="text" class="form-control form-control-user" id="email" name='email' aria-describedby="emailHelp" placeholder="Enter Email Address...">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                      <input type="password" class="form-control form-control-user" id="password" name='password' placeholder="Password">
                     </div>
-
                     <button type="submit" class="btn btn-primary btn-user btn-block">
                       Login
                     </button>
-
                   </form>
                   <hr>
                   <div class="text-center">
