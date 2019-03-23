74e2357cfe9c00e99ef842427807fad04a61770b
Ery Camel <erycamel@gmail.com>
2019-03-23 21:04:30 +0700

    video8

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -2,7 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Menu extends CI_Controller {
-
+	public function __construct()
+	{
+		parent::__construct();
+		is_logged_in();
+	}
 	public function index() 
 	{
 		$data['title'] = 'Menu Management';
