a48438b7c4bdc64513a9fc8f9cd25869598a53d3
Ery Camel <erycamel@gmail.com>
2019-03-31 20:34:02 +0700

    video10

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -11,6 +11,9 @@ class Auth extends CI_Controller {
 	
 	public function index()
 	{
+		if ($this->session->userdata('email')){
+			redirect('user');
+		}
 		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
 		$this->form_validation->set_rules('password', 'Password', 'required|trim');
 		if($this->form_validation->run() == false) {		
@@ -64,6 +67,9 @@ class Auth extends CI_Controller {
 
 	public function registration()
 	{
+		if ($this->session->userdata('email')){
+			redirect('user');
+		}
 		$data['title'] = 'WPU User Registration';
 		$this->form_validation->set_rules('name', 'Name', 'required|trim');
 		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]',[
